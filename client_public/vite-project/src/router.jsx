ca8768bf7f2400ba90b5fc203e9ed7c62aa137f8
pnnyyw <reivanielhan07@gmail.com>
2024-10-27 00:18:36 +0700

    push

---
+++ client_public/vite-project/src/router.jsx
@@ -0,0 +1,18 @@
+import { createBrowserRouter } from "react-router-dom"
+import HomePage from "./Public/HomePage"
+import DetailPage from "./Public/DetailPage"
+
+
+const Router = createBrowserRouter ([
+  {
+    path: "/pub",
+    element: <HomePage />,
+  },
+  {
+    path: "/pub/:id",
+    element: <DetailPage />,
+  }
+])
+
+export default Router
+
